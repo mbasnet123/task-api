19f6063e815bbe957100d50e91b54b91bf855e1b
mbasnet123 <mbasnet868@gmail.com>
2026-04-23 16:15:41 +0000

    implement Task database model and User API schemas

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from database import engine, Base
+import models.user
+import models.task
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
