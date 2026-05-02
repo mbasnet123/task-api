bf93d8f374963c9012127435f1ec5b5046ce8f4e
mbasnet123 <mbasnet868@gmail.com>
2026-05-02 12:29:47 +0000

    feat: add login_user in user_service

--- services/user_service.py
+++ services/user_service.py
@@ -2,6 +2,8 @@ from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from schemas.user import UserCreate
 import repositories.user_repository as user_repo
+from jose import jwt
+import os
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -13,3 +15,12 @@ def register_user(db:Session, user:UserCreate):
     new_user = UserCreate(name=user.name, email=user.email, password=hashed_password)
     return user_repo.create_user(db, new_user)
 
+def login_user(db: Session, email:str, password: str):
+    existing_user = user_repo.get_user_by_email(db, email)
+    if not existing_user:
+        raise ValueError("Email not registered")
+    password_verified = pwd_context.verify(password, existing_user.password)
+    if not password_verified:
+        raise ValueError("password not verified")
+    token = jwt.encode({"sub": existing_user.email}, os.getenv("SECRET_KEY"), algorithm=os.getenv("ALGORITHM"))
+    return token
