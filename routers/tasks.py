0466cc9718f1c773634f13fd93ac2ade52d219d2
mbasnet123 <mbasnet868@gmail.com>
2026-05-09 04:00:48 +0000

    add endpoint to update and delete task

--- routers/tasks.py
+++ routers/tasks.py
@@ -16,3 +16,17 @@ async def get_tasks(db: Session = Depends(get_db), current_user = Depends(get_cu
 async def get_task_by_id(task_id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
     task = task_service.get_task_by_id(db, current_user.id, task_id)
     return task
+
+@router.post("/tasks", response_model = TaskResponse)
+async def create_task(task: TaskCreate, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    task = task_service.create_task(db, task, current_user.id)
+    return task
+
+@router.put("/tasks/{task_id}", response_model = TaskResponse)
+async def update_task(task: TaskCreate, task_id: int,  db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    task = task_service.update_task(db, task, task_id, current_user.id)
+    return task
+
+@router.delete("/tasks/{task_id}")
+async def delete_task(task_id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    task_service.delete_task(db, current_user.id, task_id)
