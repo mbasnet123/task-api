9af1dff15baa9a9addc430057e582795e7d6742c
mbasnet123 <mbasnet868@gmail.com>
2026-05-11 17:36:31 +0000

    chore: register routes and test /auth/register endpoint

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI
 from database import engine, Base
 import models.user
 import models.task
+from routers import auth, tasks
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(tasks.router, tags=["tasks"])
 
 @app.get("/")
 async def root():
