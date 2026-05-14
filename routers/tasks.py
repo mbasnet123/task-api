9778dc77463dd8b225f9d8b21f8c9299a10956e0
mbasnet123 <mbasnet868@gmail.com>
2026-05-14 17:20:47 +0000

    feat: error handling for tasks routes

--- routers/tasks.py
+++ routers/tasks.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from database import get_db
 from schemas.task import TaskCreate, TaskResponse
@@ -9,24 +9,55 @@ router = APIRouter()
 
 @router.get("/tasks", response_model=list[TaskResponse])
 async def get_tasks(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    tasks= task_service.get_tasks(db, current_user.id)
-    return tasks
+    try:
+        tasks= task_service.get_tasks(db, current_user.id)
+        return tasks
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
+
 
 @router.get("/tasks/{task_id}", response_model=TaskResponse)
 async def get_task_by_id(task_id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    task = task_service.get_task_by_id(db, current_user.id, task_id)
-    return task
+    try:
+        task = task_service.get_task_by_id(db, current_user.id, task_id)
+        return task
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
 
 @router.post("/tasks", response_model = TaskResponse)
 async def create_task(task: TaskCreate, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    task = task_service.create_task(db, task, current_user.id)
-    return task
+    try:
+        task = task_service.create_task(db, task, current_user.id)
+        return task
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
 
 @router.put("/tasks/{task_id}", response_model = TaskResponse)
 async def update_task(task: TaskCreate, task_id: int,  db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    task = task_service.update_task(db, task, task_id, current_user.id)
-    return task
+    try:
+        task = task_service.update_task(db, task, task_id, current_user.id)
+        return task
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
 
 @router.delete("/tasks/{task_id}")
 async def delete_task(task_id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    task_service.delete_task(db, current_user.id, task_id)
+    try:
+        task_service.delete_task(db, current_user.id, task_id)
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
